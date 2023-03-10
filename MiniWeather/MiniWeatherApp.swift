841530f6035d68970e918ad0785ea19910437c2f
agentshield <anhsonleit@gmail.com>
2023-03-10 22:54:49 +0700

    Initial Commit

---
+++ MiniWeather/MiniWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniWeatherApp.swift
+//  MiniWeather
+//
+//  Created by Anh Son Le on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MiniWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
